2b428661f6d9fc5f664b85ddd67866ac2ee9e535
hamzakindi <humzahalkindi@hotmail.com>
2017-10-31 23:52:33 +0400

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heros.component';
+import { HeroService } from './hero.service';
 import { HeroDetailComponent } from './hero-detail.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
@@ -20,11 +21,15 @@ import { RouterModule } from '@angular/router';
     ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent
   ],
-  providers: [],
+  providers: [
+    HeroService
+  ],
   bootstrap: [AppComponent]
 })
 
 
-export class AppModule { }
+export class AppModule { 
+}
--- src/app/heros.component.ts
+++ src/app/heros.component.ts
@@ -2,13 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 @Component({
-  selector: 'app-my-heroes',
+  selector: 'my-heroes',
   templateUrl: './hero.component.html',
   styleUrls: ['./app.component.css'],
   providers: [HeroService]
 })
 export class HeroesComponent implements OnInit {
-  title = 'Tour of Heroes';
   selectedHero: Hero;
   heroes: Hero[];
   constructor (private heroService: HeroService) {
